10bc6e98a4fdcd47ad07f6254d411d51bafedd29
angelina-tsuboi <angelina.t1832@gmail.com>
2020-08-08 13:59:41 -0700

    vigenere encrypt working

--- app/js/vigenere.js
+++ app/js/vigenere.js
@@ -1,8 +1,6 @@
 let uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 let upperTable = [uppercaseLetters];
-let lowerTable = [lowercaseLetters];
 
 let plaintext = "ATTACKATDAWN";
 let key = "LEMON"; //set default key
@@ -16,12 +14,12 @@ $(document).ready(function() {
     $('#encodeArea').keydown(function() {
       if (event.keyCode == 13) {
         var text = $('#encodeArea').val();
-        alert(text);
         if(text == "key()" || text == "setKey()"){
             key = prompt("What should the key be?");
-            setUpKey(key);
+            setUpKey(key.toUpperCase().trim());
         }else{
-
+            let encoded = encodify(text.toUpperCase().trim());
+            $('#decodeArea').val(encoded);
         }
       }
     });
@@ -29,11 +27,34 @@ $(document).ready(function() {
     $('#decodeArea').keydown(function() {
       if (event.keyCode == 13) {
         var text = $('#decodeArea').val();
-       
+        let decoded = codify(text.toUpperCase().trim());
       }
     });
   });
 
+
+//Set up Tables
+function getLetterSet(set, index, table){
+    let first = set[0];
+    let newSet = [];
+    for(let i = 1; i <= 25; i++){
+      newSet.push(set[i])
+    }
+    newSet.push(first);
+    table.push(newSet);
+    if(index <= 23){
+      getLetterSet(newSet, index + 1, table);
+    }else{
+      return table;
+    }
+  }
+
+  let uppercaseTable = getLetterSet(uppercaseLetters, 0, upperTable);
+
+
+
+
+// Helper Methods
 
   function setUpKey(key){
     repeatKey = "";
@@ -49,3 +70,53 @@ $(document).ready(function() {
     }
 
   }
+
+  function codify(value){
+    let decoded = "";
+    let words = value.split(' ');
+    for(let word of words){
+      let decodedWord = "";
+      for(let i = 0; i < word.length; i++){
+        let letter = word[i];
+        let cipher = "";
+        if(uppercaseLetters.includes(letter)){
+          
+        }else{
+          cipher = letter;
+        }
+  
+        decodedWord += cipher;
+      }
+      decoded += `${decodedWord} `
+    }
+  
+    return decoded;
+  }
+  
+  
+  function encodify(value){
+    let encoded = "";
+    let words = value.split(' ');
+    for(let word of words){
+      let encodedWord = "";
+      for(let i = 0; i < word.length; i++){
+        let letter = word[i];
+        let keyLetter = repeatKey[i];
+        let cipher = "";
+        let rowNum;
+        let colNum;
+        if(uppercaseLetters.includes(letter)){
+            rowNum = uppercaseLetters.indexOf(keyLetter);
+            colNum = uppercaseLetters.indexOf(letter);
+            cipher = upperTable[rowNum][colNum];
+        }else{
+          cipher = letter;
+        }
+  
+        encodedWord += cipher;
+      }
+      encoded += `${encodedWord} `
+    }
+    return encoded;
+  }
+  
